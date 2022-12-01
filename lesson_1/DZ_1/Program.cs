f1f2eb5ec9b3678d2dddb10aa5ee3049bdec5a96
unknown <ykka01@mail.ru>
2022-12-01 23:19:14 +0500

    1 задача

---
+++ lesson_1/DZ_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+string num1 = Console.ReadLine()!;
+int num1int = int.Parse(num1);
+
+Console.WriteLine("Введите второе число");
+string num2 = Console.ReadLine()!;
+int num2int = int.Parse(num2);
+
+if (num1int>num2int)
+{
+    Console.WriteLine(num1int + "больше" + num2int);
+}
+
+else if (num2int > num1int)
+{
+    Console.WriteLine(num2int + "больше" + num1int);
+}
+
+else 
+{
+Console.WriteLine("числа равны");   
+}

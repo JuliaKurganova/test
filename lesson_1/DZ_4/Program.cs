7ca4e874d881a921cd5380e8536b4d7d22d934de
unknown <ykka01@mail.ru>
2022-12-02 21:30:47 +0500

    4 задача

---
+++ lesson_1/DZ_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" Введите целое число больше нуля ");
+string num1 = Console.ReadLine()!;
+int num1int = int.Parse(num1);
+
+for (int i = 1; i <= num1int ; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine( i );
+    } 
+}

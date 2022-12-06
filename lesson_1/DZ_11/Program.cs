da67e715cfb882bfe2096b319ee227ecdd23ef34
unknown <ykka01@mail.ru>
2022-12-06 12:16:25 +0500

    11 задача

---
+++ lesson_1/DZ_11/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите трёхзначное число");
+int num1 = int.Parse(Console.ReadLine()!);
+if ((num1 >= 100) && (num1 < 1000))
+{
+    int firestDigit = num1 % 100;
+    int secondDigit = firestDigit / 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Введено не трёхзначное число");
+}

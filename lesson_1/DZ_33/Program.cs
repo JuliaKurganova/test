b5d216e378bc58a69dffc9c74f1d8e04b9e28faa
unknown <ykka01@mail.ru>
2022-12-06 12:52:58 +0500

    33 задача

---
+++ lesson_1/DZ_33/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер дня недели (1-7)");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if (num1 > 0 && num1 < 8)
+{
+    if (num1 == 6 ||  num1 == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Буднии");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели чило выходящее за диапозон от 1 до 7");
+}

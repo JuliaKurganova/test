4ed1572b4fc0d394879699160619361432c2270b
unknown <ykka01@mail.ru>
2022-12-06 12:37:16 +0500

    22 задача

---
+++ lesson_1/DZ_22/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число больше 99");
+string num1 = Console.ReadLine()!;
+
+if ( num1[0]== '-')
+{
+    if (num1.Length >= 4)
+    {
+        Console.WriteLine(num1[3]);
+    }
+    else
+    {
+        Console.WriteLine(" число менее трёх знаков ");
+    }
+}
+else
+{
+    if (num1.Length >= 3)
+    {
+         Console.WriteLine(num1[2]);
+    }
+    else
+    {
+        Console.WriteLine(" число менее трёх знаков ");
+    }
+}

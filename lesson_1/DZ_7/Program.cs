f9d95ac3d8e0937f13cf466c7cfa8b3439273506
unknown <ykka01@mail.ru>
2022-12-09 15:49:02 +0500

    7 задача

---
+++ lesson_1/DZ_7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число, ввод знака '-' не учитывается");
+int N = Math.Abs(int.Parse(Console.ReadLine()!));
+
+for (int i = 1 ; i <= N ; i ++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+} 

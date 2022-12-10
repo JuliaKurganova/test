4af33726d8b4b96dd7822a9d5313538aee0a883a
unknown <ykka01@mail.ru>
2022-12-10 18:18:40 +0500

    9 задача

---
+++ lesson_1/DZ_9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int digitFunction = Sumdigit(num);
+Console.WriteLine(digitFunction);
+
+int Sumdigit(int number)
+{
+    int sum = 0;
+    while (number >= 10)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum + number;
+}

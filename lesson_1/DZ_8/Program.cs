0eb814a0268f10f5ccef83656d24cb71335cb828
unknown <ykka01@mail.ru>
2022-12-10 18:15:22 +0500

    8 задача

---
+++ lesson_1/DZ_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("A= ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("B= ");
+int numB = int.Parse(Console.ReadLine()!);
+
+
+int expFunction = Power(numA, numB);
+Console.WriteLine(expFunction);
+
+
+int Power(int numberA, int numberB)
+{
+    int expAinB = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        expAinB = expAinB * numberA;
+    }
+    return expAinB;
+}

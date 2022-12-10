770000eec8d6afb0fe6b13b751c1da6c9fc8a28b
unknown <ykka01@mail.ru>
2022-12-10 18:20:19 +0500

    10 задача

---
+++ lesson_1/DZ_10/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число n");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число min");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число max ");
+int max = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[num];
+
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}

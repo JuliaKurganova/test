68da37411a8f13224cceebbf33cd56ebb5627db7
unknown <ykka01@mail.ru>
2022-12-14 02:54:41 +0500

    13 задача

---
+++ lesson_1/DZ_13/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Введите число n");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число min");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число max ");
+int max = int.Parse(Console.ReadLine()!);
+ 
+
+double[] a2 =FillArray(num, min, max);
+double counts = minmax(a2);
+PrintArray(a2);
+Console.WriteLine(" Число " + counts);
+
+
+double[] FillArray(int msz, int fmin, int fmax)
+{
+  double[] field = new double[msz]; 
+   for (int i = 0; i < field.Length; i++)
+    {
+        field[i] = new Random().Next(fmin, fmax + 1);
+    }
+    return field;
+}
+
+double minmax(double[] arr) 
+{
+     double minFind =100;
+     double maxFind = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(minFind > arr[i])
+        {
+            minFind = arr[i];
+        }
+         if(maxFind < arr[i])
+        {
+            maxFind = arr[i];
+        }
+    }
+    return maxFind - minFind;
+}
+
+void PrintArray(double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+

b96917f746b345abd6c9e92c0418c7f173be47f0
unknown <ykka01@mail.ru>
2022-12-21 00:54:37 +0500

    18 задача

---
+++ lesson_1/DZ_18/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте двумерный массив размером m×n.
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+ 
+int[,] matrix = Create2DArray(m, n, -5, 10);
+Print2DArray(matrix);
+ 
+ int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+ 
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}

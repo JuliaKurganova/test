1116d1febc9a2fbddeb4393bad0c9e8d9bf025b3
unknown <ykka01@mail.ru>
2022-12-26 23:18:28 +0500

    66 задача

--- lesson_1/DZ_66/Program.cs
+++ lesson_1/DZ_66/Program.cs
@@ -3,46 +3,15 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int m = InputInt("Введите M:");
-int n = InputInt("Введите N:");
-Console.WriteLine($" {CountNaturalSum(m, n)}");//M = {m}; N = {n} ->
 
-int InputInt (string output)
+int n, m, s=0;
+Console.Write("Введите M : ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите N : ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+     s += i;
 }
+Console.WriteLine(s);
 
-int CountNaturalSum(int m, int n)
-{
-    if (m == n)
-    {
-        return n;
-    }
-    else
-    {
-    return n + CountNaturalSum(m, n - 1);
-    }
-}
-
-
-
-/*Console.WriteLine("Введите M:");
-int m = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите N:");
-int n = int.Parse(Console.ReadLine());
-
-Console.Write(FromMtoN( 4, 8));
-string FromMtoN( int m, int n ) 
-{
-    string rezult = string.Empty;
-    if (m == n)
-    {
-        return rezult + m.ToString();
-    }
-    else
-    {
-        return rezult + n.ToString() + FromMtoN(m, n - 1);
-    }
-     
-}*/
